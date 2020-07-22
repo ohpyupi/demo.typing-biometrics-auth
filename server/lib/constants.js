93baa0340bf7cdb5e365f6c9b7663ed0f1c99991
Hoseong Lee <ohpyupi@gmail.com>
2020-07-21 23:43:35 -0700

    Added constant variable AUTH_STATUS in the constant lib module

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -1,3 +1,7 @@
 module.exports = {
   EMAIL_REGEX: /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/,
+  AUTH_STATUS: {
+    LOGGED_IN: 'logged_in',
+    PARTIALLY_AUTHENTICATED: 'partially_authenticated',
+  },
 };
