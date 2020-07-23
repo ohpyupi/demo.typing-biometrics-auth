c8099cae2ed783f5570a9143026f45f89bf6f7ea
Hoseong Lee <ohpyupi@gmail.com>
2020-07-23 00:46:49 -0700

    Updated the email regex

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  EMAIL_REGEX: /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/,
+  EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   AUTH_STATUS: {
     LOGGED_IN: 'logged_in',
     PARTIALLY_AUTHENTICATED: 'partially_authenticated',
