58215762ad661d258c922a2a37eeb51cb8e17111
supergentle <ohpyupi@gmail.com>
2020-07-24 00:09:09 -0700

    Nodemailer uses Google OAuth2 tokens (#14)
    * Intalled the googleapis npm module
    
    * Nodemailer now uses the google oauth2 tokens to authenticate the sender email

--- server/services/email/index.js
+++ server/services/email/index.js
@@ -1,18 +1,44 @@
 const nodemailer = require('nodemailer');
 const {
+  google: {
+    auth: {
+      OAuth2,
+    },
+  },
+} = require('googleapis');
+const {
   BASE_URL,
   GMAIL_EMAIL,
-  GMAIL_PASSWORD,
-  EMAIL_SENDER,
+  EMAIL_PROVIDER_HOST,
+  EMAIL_PROVIDER_PORT,
+  GOOGLE_OAUTH2_CLIENT_ID,
+  GOOGLE_OAUTH2_SECRET,
+  GOOGLE_OAUTH2_REDIRECT_URL,
+  GOOGLE_OAUTH2_REFRESH_TOKEN,
 } = require('../../../config/variables');
 const { Challenge } = require('../../models/challenge');
 
 const sendEmail = async (options) => {
+  const oauth2Client = new OAuth2(
+    GOOGLE_OAUTH2_CLIENT_ID,
+    GOOGLE_OAUTH2_SECRET,
+    GOOGLE_OAUTH2_REDIRECT_URL,
+  );
+  oauth2Client.setCredentials({
+    refresh_token: GOOGLE_OAUTH2_REFRESH_TOKEN,
+  });
+  const accessToken = await oauth2Client.getAccessToken();
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: EMAIL_PROVIDER_HOST,
+    port: EMAIL_PROVIDER_PORT,
+    secure: true,
     auth: {
+      type: 'OAuth2',
       user: GMAIL_EMAIL,
-      pass: GMAIL_PASSWORD,
+      clientId: GOOGLE_OAUTH2_CLIENT_ID,
+      clientSecret: GOOGLE_OAUTH2_SECRET,
+      refreshToken: GOOGLE_OAUTH2_REFRESH_TOKEN,
+      accessToken,
     },
   });
   return transporter.sendMail(options);
@@ -24,7 +50,7 @@ const sendEmailChallengeCode = async ({ _id: userId, email }) => {
   });
   await challenge.save();
   await sendEmail({
-    from: EMAIL_SENDER,
+    from: GMAIL_EMAIL,
     to: email,
     subject: 'Please enter the challenge code to log in.',
     text: `Please enter the challenge code: ${challenge.code}`,
@@ -37,7 +63,7 @@ const sendEmailConfirmDeviceLink = async ({ email }, { _id: deviceId }) => {
   });
   await challenge.save();
   await sendEmail({
-    from: EMAIL_SENDER,
+    from: GMAIL_EMAIL,
     to: email,
     subject: 'New device login is attempted!',
     text: `Please approve your device by clicking : ${BASE_URL}/confirm/device/${challenge.code}`,
@@ -50,7 +76,7 @@ const sendEmailVerificationLink = async ({ _id: userId, email }) => {
   });
   await challenge.save();
   await sendEmail({
-    from: EMAIL_SENDER,
+    from: GMAIL_EMAIL,
     to: email,
     subject: 'Please verify your email',
     text: `Please verify your email by clicking ${BASE_URL}/confirm/email/${challenge.code}`,
